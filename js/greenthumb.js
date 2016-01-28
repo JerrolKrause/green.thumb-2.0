7b0d79c2857c21430987e34f85f0616f6b98370e
Jerrol Krause <jerrolkrause@gmail.com>
2016-01-28 13:51:26 -0800

    Finished migrating calendar controller, successfully building tasks object

--- js/greenthumb.js
+++ js/greenthumb.js
@@ -9,16 +9,15 @@ window.greenthumb = (function () {
     greenThumb.factory("gtGetData", function ($http, $rootScope) {
         
         var data = {
-            params              : {                     //Holds application parameters
-                dates          : (function(){
+            params              : {                         //Holds application parameters
+                dates          : (function(){               //Generates the main dates used by the app
                     var today = moment();
                     var obj = {
-                        main        : today.clone(), //This is the user adjustable date
+                        main        : today.clone(),        //This is the user adjustable date
                         main_pos    : Math.round((((today.format("M") - 1) * 8.333) + ((today.format("D") / today.daysInMonth()) * 8.333)) * 10) / 10,
-                        today       : today, //Always holds todays date
+                        today       : today,                //Always holds todays date
                         today_pos   : Math.round((((today.format("M") - 1) * 8.333) + ((today.format("D") / today.daysInMonth()) * 8.333)) * 10) / 10
                     };
-                    console.log(obj);
                     return obj;
                 })()
             },                   
@@ -30,9 +29,10 @@ window.greenthumb = (function () {
         //Create new instances
         data.create = {
             
+            
             /**
              * Creates a new garden object
-             * @param {type} garden
+             * @param {type} garden - Garden JSON object
              * @returns {undefined}
              */
             garden: function (garden) {
@@ -40,38 +40,64 @@ window.greenthumb = (function () {
                 //Load this garden's attributes
                 self.label          = garden.label;
                 
-                //Set the spring frost date and position
-                self.frost_spring   = {
-                    date            : moment().set('month', garden.options.frost_spring.month).set('date', garden.options.frost_spring.date)
-                };
-                self.frost_spring.position = Math.floor(self.frost_spring.date.format('DDD') * 100 / 365 * 10) / 10;
+                //Check if the frost_spring date is present and has been set
+                if (angular.isDefined(garden.options.frost_spring) && angular.isDefined(garden.options.frost_spring.month)) {
+                    //Set the spring frost date and position
+                    self.frost_spring = {
+                        date: moment().set('month', garden.options.frost_spring.month).set('date', garden.options.frost_spring.date)
+                    };
+                    self.frost_spring.position = Math.floor(self.frost_spring.date.format('DDD') * 100 / 365 * 10) / 10;
+                } else {
+                    self.frost_spring = 'Not Set';
+                }
                 
-                //Set the fall frost date and position
-                self.frost_fall     = {
-                    date            : moment().set('month', garden.options.frost_fall.month).set('date', garden.options.frost_fall.date)
-                };
-                self.frost_fall.position     = Math.floor((365 - self.frost_fall.date.format('DDD')) * 100 / 365 * 10) / 10;
-                        
+                //Check if the frost_fall date is present and has been set
+                if (angular.isDefined(garden.options.frost_fall) && angular.isDefined(garden.options.frost_fall.month)) {
+                    //Set the fall frost date and position
+                    self.frost_fall = {
+                        date: moment().set('month', garden.options.frost_fall.month).set('date', garden.options.frost_fall.date)
+                    };
+                    self.frost_fall.position = Math.floor((365 - self.frost_fall.date.format('DDD')) * 100 / 365 * 10) / 10;
+                } else {
+                    self.frost_fall = 'Not Set';
+                }
+                    
                 //Set the zone        
-                self.zone           = garden.options.zone;
+                self.zone           = garden.options.zone || 'Not Set';
                 
                 //Create container to hold the areas
                 self.areas          = [];
+                
+                self.tasks          = {};
+                
                 //Set the active/viewable garden to this garden, the newest created garden will always be referenced first
                 data.activeGarden   = self;
                 
-                //If this garden has areas, load them up
+                /**
+                 * Creates a new area for this garden
+                 * This is created on a garden level which allows us to trickle up information from the area level
+                 * @param {type} areaObj
+                 * @returns {undefined}
+                 */
+                self.addArea = function(areaObj){
+                    var area = new data.create.area(areaObj);
+                    self.areas.push(area);
+                };
+                
+                //If this garden has areas on load, create them automatically
                 if(garden.areas.length > 0){
                     angular.forEach(garden.areas, function(value){
-                        var area = new  data.create.area(value);
-                        self.areas.push(area);
+                        self.addArea(value);
                     });
                 }
+                
+                return self;
             },//end create.garden
+            
             
             /**
              * Create a new area object
-             * @param {type} area
+             * @param {obj} area - The area object specified by the user model
              * @returns {undefined}
              */
             area : function(area){
@@ -80,67 +106,191 @@ window.greenthumb = (function () {
                 self.label          = area.label;
                 self.width          = area.width;
                 self.length         = area.length;
-                self.produce        = [];
+                self.produce        = [];               //Holds all the produce objects
+                self.seasons        = {                 //Holds the seasons flag. This will tell us what seasons are planted in this bed for filtering. Sets default to false
+                    spring  :   false,
+                    summmer :   false,
+                    fall    :   false,
+                    winter  :   false
+                }; 
                 
-                 //If this area has produce, load them up
+                /**
+                 * Adds a new produce item to this area
+                 * Having a closure allows us to trickle up information from the produce to the area level
+                 * @param {type} produceObj
+                 * @returns {undefined}
+                 */
+                self.addProduce = function (produceObj) {
+                    var produce = new data.create.produce(produceObj);
+                    //Add new produce item to the produce array
+                    self.produce.push(produce);
+                    //Update this area's season flag to true to make filtering possible on an area level
+                    self.seasons[produce.season] = true;
+                };
+                
+                //If this area has produce on default load, create all the entries
                 if(area.produce.length > 0){
                     angular.forEach(area.produce, function(value){
-                        var produce = new  data.create.produce(value);
-                        self.produce.push(produce);
+                        self.addProduce(value);
                     });
                 }
-            },
+                
+                return self;
+            },//end create.area
+            
             
             /**
              * Create a new produce object
-             * @param {type} produce
-             * @returns {undefined}
+             * @param {obj} produce - A user specified produce object from the model
+             * @returns {undefined} - The fully generated produce file
              */
             produce : function(produce){
-                var content;
+                var self = this;
+                
                 //Check if this produce item has a parent
                 if ('parent' in data.produce[produce.id]){
                     //Get the parent ID
                     var parentID = data.produce[produce.id].parent;
                     //Copy the content from the parent produce object
-                    content = angular.copy(data.produce[parentID]);
+                    //self = angular.copy(data.produce[parentID]);
+                    angular.extend(self, data.produce[parentID]);
                     //Now overwrite any of the default parent content with the child produce content
-                    content = angular.extend(content, data.produce[produce.id]);
+                    self = angular.extend(self, data.produce[produce.id]);
                     //Set image class
-                    content.img = parentID;
+                    self.img = parentID;
+                    self.label_parent = data.produce[parentID].label;
                 //If this item does not have a parent    
                 } else {
                     //Just load the content right in
-                    content = angular.copy(data.produce[produce.id]);
+                    //self = angular.copy(data.produce[produce.id]);
+                     angular.extend(self, data.produce[produce.id]);
                     //Set image class
-                    content.img = produce.id;
+                    self.img = produce.id;
                 }
                 
                 //Figure out this produce items set dates
                 var plantMe = moment().set('month', produce.plantDate.month).set('date', produce.plantDate.date);
-                content.dates = {
-                    seedlings           : plantMe.clone().subtract(content.seedling, 'weeks'),
+                self.dates = {
+                    seedlings           : plantMe.clone().subtract(self.seedling, 'weeks'),
                     plant               : plantMe,
-                    harvest_start       : plantMe.clone().add(content.maturity, 'days'),
-                    harvest_complete    : plantMe.clone().add((content.harvest * 7) + content.maturity, 'days')
+                    harvest_start       : plantMe.clone().add(self.maturity, 'days'),
+                    harvest_complete    : plantMe.clone().add((self.harvest * 7) + self.maturity, 'days')
                 };
                 
                 //Create parameters needed by the DOM elements, used for width and positioning of calendar items
-                content.dom = {
-                    wSeedlings          : Math.round(content.seedling * 7 * 100 / 365 * 10) / 10 + 1,
-                    wGrowing            : Math.round(content.maturity * 100 / 365 * 10) / 10,
-                    wHarvesting         : Math.round(content.harvest * 7 * 100 / 365 * 10) / 10 + 1,
-                    pSeedlings          : Math.round((((content.dates.seedlings.format("M") - 1) * 8.333) + ((content.dates.seedlings.format("D") / content.dates.seedlings.daysInMonth()) * 8.333)) * 10) / 10,
-                    pGrowing            : Math.round((((content.dates.plant.format("M") - 1) * 8.333) + ((content.dates.plant.format("D") / content.dates.plant.daysInMonth()) * 8.333)) * 10) / 10,
-                    pHarvesting         : Math.round((((content.dates.harvest_start.format("M") - 1) * 8.333) + ((content.dates.harvest_start.format("D") / content.dates.harvest_start.daysInMonth()) * 8.333)) * 10) / 10 - 1
+                self.dom = {
+                    wSeedlings          : Math.round(self.seedling * 7 * 100 / 365 * 10) / 10 + 1,
+                    wGrowing            : Math.round(self.maturity * 100 / 365 * 10) / 10,
+                    wHarvesting         : Math.round(self.harvest * 7 * 100 / 365 * 10) / 10 + 1,
+                    pSeedlings          : Math.round((((self.dates.seedlings.format("M") - 1) * 8.333) + ((self.dates.seedlings.format("D") / self.dates.seedlings.daysInMonth()) * 8.333)) * 10) / 10,
+                    pGrowing            : Math.round((((self.dates.plant.format("M") - 1) * 8.333) + ((self.dates.plant.format("D") / self.dates.plant.daysInMonth()) * 8.333)) * 10) / 10,
+                    pHarvesting         : Math.round((((self.dates.harvest_start.format("M") - 1) * 8.333) + ((self.dates.harvest_start.format("D") / self.dates.harvest_start.daysInMonth()) * 8.333)) * 10) / 10 - 1
                 };
                 
+                //Figure out what season for this produce object is planted in
+                //Spring Planting
+                if (self.dates.plant.isBetween(moment().month('March').subtract(1, 'month'), moment().month('May').date(31).add(1, 'month'), 'month')) {
+                    self.season = 'spring';
+                    //Summer Planting    
+                } else if (self.dates.plant.isBetween(moment().month('June').subtract(1, 'month'), moment().month('August').date(31).add(1, 'month'), 'month')) {
+                    self.season = 'summer';
+                    //Fall Planting    
+                } else if (self.dates.plant.isBetween(moment().month('September').subtract(1, 'month'), moment().month('November').date(30).add(1, 'month'), 'month')) {
+                    self.season = 'fall';
+                    //Winter Planting    
+                } else if (self.dates.plant.isBetween(moment().month('November').subtract(1, 'month'), moment().month('February').add(1, 'month').add(1, 'year'), 'month') || self.dates.plant.isBetween(moment().month('January').subtract(1, 'month'), moment().month('Feb').add(1, 'month'), 'month')) {
+                    self.season = 'winter';
+                }
+                
+                data.create.tasks(self);
+                
                 //Set the value of this produce entry to the content
-                return content;
+                return self;
+            },//end create.produce
+            
+            
+            /**
+             * 
+             * @param {type} produce
+             * @returns {undefined}
+             */
+            tasks : function(produce){
+                //console.log(produce);
+                
+                angular.forEach(produce.dates, function(value,key){
+                    var str;
+
+                    //Figure out which activity type this is, set appropriate string for output in task pane
+                    switch (key) {
+                        case 'seedlings':
+                            str = 'Start seedlings for ' ;
+                            if (produce.directSow === true) {
+                                str = 'Direct sow ' + produce.plantCount + ' ' + produce.label + ' ' + produce.label_parent + ' seeds';
+                            }
+                            break;
+                        case 'plant':
+                            str = 'Plant ';
+                            break;
+                        case 'harvest_start':
+                            str = 'Start harvesting ';
+                            break;
+                        case 'harvest_complete':
+                            str = 'Complete harvesting ';
+                            break;
+                    }
+                    
+                    if(produce.plantCount){
+                        str += produce.plantCount + ' ';
+                    }
+                    
+                    str +=  produce.label + ' ';
+                    
+                    if(produce.label_parent){
+                        str += produce.label_parent ;
+                    }
+                    
+                    //Obj to hold data to embed in dates object
+                    var obj = {
+                        label: str,
+                        slug: key
+                    };
+                    
+                    if (!angular.isObject(data.activeGarden.tasks[value.format("YYYYMMDD")])) {
+                        data.activeGarden.tasks[value.format("YYYYMMDD")] = {
+                            label: value.format("dddd, MMMM Do"),
+                            items: [],
+                            date : value
+                        };
+                    }
+                    
+                    data.activeGarden.tasks[value.format("YYYYMMDD")].items.push(str);
+                    //console.log(obj);
+                    
+                });
+                
+                
+                /*
+                 //Obj to hold data to embed in dates object
+                 var obj = {
+                 label: str,
+                 slug: key
+                 };
+                 
+                 //Make sure the date object exists, if not create it
+                 if (!angular.isObject(taskObj[dateItems.date.format("YYYYMMDD")])) {
+                 taskObj[dateItems.date.format("YYYYMMDD")] = {
+                 label: dateItems.date.format("dddd, MMMM Do"),
+                 items: []
+                 };
+                 }
+                 */
+                
+                
             }
             
-
         };//end create
+       
+        
         
         //Fetch JSON object of garden to use
         //This call will be replaced by a CMS when that component is ready
@@ -162,7 +312,7 @@ window.greenthumb = (function () {
     
     greenThumb.controller('gtSchedule', function ($scope, gtGetData) {
         $scope.$on('dataPassed', function () {
-            
+         
         });
     });
     
@@ -177,7 +327,7 @@ window.greenthumb = (function () {
             $scope.main_pos         = gtGetData.params.dates.main_pos;
             $scope.today_pos        = gtGetData.params.dates.today_pos;
             
-            console.log(gtGetData.activeGarden);
+            console.log(gtGetData.activeGarden.tasks);
         });
         
         
--- js/model.js
+++ js/model.js
@@ -3,7 +3,6 @@
         "options": {
             "plantOnSat": true,
             "capHarvest": true,
-            "zone": "10a",
             "frost_spring": {"month": 3, "date": 1},
             "frost_fall": {"month": 9, "date": 1}
         },
@@ -13,11 +12,11 @@
                 "width": 1.5,
                 "length": 5,
                 "produce": [
-                    {"id": "melon_earlichamp",          "plantDate": {"month": 1, "date": 1}, "overrides": {}},
+                    {"id": "melon_earlichamp",          "plantDate": {"month": 7, "date": 1}, "overrides": {}},
                     {"id": "melon_earlichamp",          "plantDate": {"month": 2, "date": 1}, "overrides": {}},
-                    {"id": "sweet_potato",              "plantDate": {"month": 3, "date": 1}, "overrides": {}},
-                    {"id": "tomato",                    "plantDate": {"month": 4, "date": 1}, "overrides": {}},
-                    {"id": "tomato_big_beef",           "plantDate": {"month": 5, "date": 1}, "overrides": {}}
+                    {"id": "sweet_potato",              "plantDate": {"month": 7, "date": 1}, "overrides": {}},
+                    {"id": "tomato",                    "plantDate": {"month": 2, "date": 1}, "overrides": {}},
+                    {"id": "tomato_big_beef",           "plantDate": {"month": 1, "date": 1}, "overrides": {}}
                 ]
             },
              {
@@ -25,10 +24,10 @@
                 "width": 1.5,
                 "length": 5,
                 "produce": [
-                    {"id": "melon_earlichamp",          "plantDate": {"month": 6, "date": 1}, "overrides": {}},
-                    {"id": "sweet_potato",              "plantDate": {"month": 7, "date": 1}, "overrides": {}},
-                    {"id": "tomato",                    "plantDate": {"month": 8, "date": 1}, "overrides": {}},
-                    {"id": "tomato_big_beef",           "plantDate": {"month": 9, "date": 1}, "overrides": {}}
+                    {"id": "melon_earlichamp",          "plantDate": {"month": 1, "date": 1}, "overrides": {}},
+                    {"id": "sweet_potato",              "plantDate": {"month": 2, "date": 1}, "overrides": {}},
+                    {"id": "tomato",                    "plantDate": {"month": 9, "date": 1}, "overrides": {}},
+                    {"id": "tomato_big_beef",           "plantDate": {"month": 2, "date": 1}, "overrides": {}}
                 ]
             }
         ]
